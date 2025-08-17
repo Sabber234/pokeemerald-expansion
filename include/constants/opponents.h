a9b907c8b2fb24a9a18848efdf91ecf4b49fb46e
Sabber234 <Sebastianbernhardtss@gmail.com>
2025-08-17 13:10:28 +0200

    Finished video tutorial
    Let's see if this works

--- include/constants/opponents.h
+++ include/constants/opponents.h
@@ -859,10 +859,14 @@
 #define TRAINER_BRENDAN_PLACEHOLDER         853
 #define TRAINER_MAY_PLACEHOLDER             854
 <<<<<<< HEAD
+<<<<<<< HEAD
 #define ROUTE101_FLANNERY                  855
 =======
 #define TRAINER_FLANNERY_6                  855
 >>>>>>> 917cf5f59b (idk if this works)
+=======
+#define ROUTE101_FLANNERY                  855
+>>>>>>> 0271bf45d4 (Finished video tutorial)
 
 // NOTE: Because each Trainer uses a flag to determine when they are defeated, there is only space for 9 additional trainers before trainer flag space overflows
 //       More space can be made by shifting flags around in constants/flags.h or changing how trainer flags are handled
